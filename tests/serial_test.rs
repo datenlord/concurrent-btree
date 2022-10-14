a3f00f9cb1ca9caa75391c43df6be3966da7e06a
HeimingZ <zhuhmch@qq.com>
2022-10-14 11:08:13 +0800

    use Arc to store keys and values

--- tests/serial_test.rs
+++ tests/serial_test.rs
@@ -14,11 +14,11 @@ fn test_serial_insert() {
     assert_eq!(100002, tree.len());
     // get
     for i in 0..100000 {
-        assert_eq!(Some(i), tree.get(&i));
+        assert_eq!(i, *tree.get(&i).unwrap());
     }
     // insert and get old value
     for i in 0..100000 {
-        assert_eq!(Some(i), tree.insert(i, 100000 - i));
+        assert_eq!(i, *tree.insert(i, 100000 - i).unwrap());
     }
     assert_eq!(100002, tree.len());
 }
@@ -37,11 +37,11 @@ fn test_serial_remove() {
     assert_eq!(100002, tree.len());
     // get
     for i in 0..100000 {
-        assert_eq!(Some(i), tree.get(&i));
+        assert_eq!(i, *tree.get(&i).unwrap());
     }
     // remove
     for i in 0..50000 {
-        assert_eq!(Some(i), tree.remove(&i));
+        assert_eq!(i, *tree.remove(&i).unwrap());
     }
     assert_eq!(50002, tree.len());
 }
